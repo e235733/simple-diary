b8662c3635737356860058b10a9d940d1802d736
e235733@ie.u-ryukyu.ac.jp <hamzayunta17@icloud.com>
2025-12-22 03:26:41 +0900

    feature: add button, diary list, diary text, edit button, delete button

--- views/diary_screen.py
+++ views/diary_screen.py
@@ -16,43 +16,53 @@ class DiaryScreen(tk.Frame):
         self.list_operation_frame = tk.Frame(self, bg="black")
         self.list_operation_frame.grid(row=0, column=0, sticky="ns")
         # 1列目に対して広がりを許可し、0列目,0行目に対しては許可しない
-        self.list_operation_frame.grid_columnconfigure(0, weight=0, minsize=300)
+        self.list_operation_frame.grid_columnconfigure(0, weight=0, minsize=200)
         self.list_operation_frame.grid_rowconfigure(0, weight=0)
         self.list_operation_frame.grid_rowconfigure(1, weight=1)
         
-        # ボタンフレームを作成
-        self.add_button_frame = tk.Frame(self.list_operation_frame, height=50, bg="red")
+        # ボタンを作成
+        self.add_button_frame = tk.Frame(self.list_operation_frame, bg="red")
         self.add_button_frame.grid(row=0, column=0, sticky="ew")
+        self.add_button = tk.Button(self.add_button_frame, text="日記を追加")
+        self.add_button.pack(fill="both", expand=True)
 
-        #　リストフレームを作成
+        # リストを作成
         self.diary_list_frame = tk.Frame(self.list_operation_frame, bg="blue")
         self.diary_list_frame.grid(row=1, column=0, sticky="nsew")
+        self.diary_list = tk.Listbox(self.diary_list_frame, selectmode="browse")
+        self.diary_list.pack(fill="both", expand=True)
+        # テストデータ
+        for i in range(20):
+            self.diary_list.insert(tk.END, f"test{i+1}")
 
     # 日記の表示エリアを作成
     def make_diary_show(self):
-        self.diary_show_frame = tk.Frame(self, bg="white")
-        self.diary_show_frame.grid(row=0, column=1, sticky="nsew")
-        # 0行目,0列目に対して広がりを許可し、1行目に対しては許可しない
-        self.diary_show_frame.grid_rowconfigure(0, weight=1)
-        self.diary_show_frame.grid_columnconfigure(0, weight=1)
-        self.diary_show_frame.grid_rowconfigure(1, weight=0)
-
-        # 内容表示フレームを作成
-        self.dialy_text_frame = tk.Frame(self.diary_show_frame, bg="pink")
-        self.dialy_text_frame.grid(row=0, column=0, sticky="nsew")
+        self.diary_text_frame = tk.Frame(self, bg="white")
+        self.diary_text_frame.grid(row=0, column=1, sticky="nsew")
+        self.diary_text = tk.Text(self.diary_text_frame)
+        self.diary_text.pack(fill="both", expand=True)
+        # テストデータ
+        self.diary_text.insert(tk.END, "hello, world!")
         
         # 日記操作フレームを作成
-        self.text_operation_frame = tk.Frame(self.diary_show_frame, bg="gray")
-        self.text_operation_frame.grid(row=1, column=0, sticky="ew")
-        # 全てのグリッドに広がりを許可する
-        self.text_operation_frame.grid_rowconfigure(0, weight=1, minsize=50)
-        self.text_operation_frame.grid_columnconfigure(0, weight=1)
-        self.text_operation_frame.grid_columnconfigure(1, weight=1)
+        self.text_operation_frame = tk.Frame(self, bg="gray")
+        self.text_operation_frame.place(relx=1, rely=1, anchor="se")
+        self.text_operation_frame.propagate(False)
+        # 行には広がりを許可せず、列には許可する
+        self.text_operation_frame.grid_rowconfigure(0, weight=0)
+        self.text_operation_frame.grid_columnconfigure(0, weight=1, minsize=200)
+        self.text_operation_frame.grid_columnconfigure(1, weight=1, minsize=200)
 
         # 編集ボタンフレームを作成
         self.edit_button_frame = tk.Frame(self.text_operation_frame, bg="skyblue")
         self.edit_button_frame.grid(row=0, column=0, sticky="nsew")
+        self.edit_button = tk.Button(self.edit_button_frame, text="編集")
+        self.edit_button.pack(fill="both")
 
         # 削除ボタンフレームを作成
         self.delete_button_frame = tk.Frame(self.text_operation_frame, bg="lightblue")
         self.delete_button_frame.grid(row=0, column=1, sticky="nsew")
+        self.delete_button = tk.Button(self.delete_button_frame, text="削除")
+        self.delete_button.pack(fill="both")
+
+    
